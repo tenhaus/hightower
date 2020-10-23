96f282234a355a50fd444a4eb8796b03d6cd1104
Chris Hayen <chayen@gmail.com>
2020-10-23 08:16:50 -0400

    Better long desc.

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,9 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "ht",
 	Short: "yama",
-	Long:  "Hightower - something something something",
+	Long: `The project aims to make an open source, multi-language, local development environment
+that prioritizes role-based startup, service dependencies, testing and config generation
+while remaining useable and unopinionated.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("run")
 	},
