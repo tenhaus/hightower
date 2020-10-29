440564de9195de29be83b3247dedd04f44e6f999
Chris Hayen <chayen@gmail.com>
2020-10-29 08:12:12 -0400

    Better fs.walk + tests

--- pkg/fs/walk.go
+++ pkg/fs/walk.go
@@ -4,20 +4,39 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // Walk returns a list of all files in the given directory
-func Walk(dir string) ([]string, error) {
+func Walk(dir string, includeDir bool) ([]string, error) {
 	var files []string
+	cleanDir := dir
 
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+	// Strip out ./ from the beginning of path
+	// if it exists
+	if strings.Index(dir, "./") == 0 {
+		cleanDir = dir[2:]
+	}
+
+	// Build a list of files from cleanDir
+	err := filepath.Walk(cleanDir, func(path string, info os.FileInfo, err error) error {
+
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		files = append(files, path+info.Name())
+		// We don't have to include the root directory
+		if cleanDir == path {
+			return nil
+		}
 
-		return err
+		// Don't include directories
+		if !includeDir && info.IsDir() {
+			return nil
+		}
+
+		files = append(files, path)
+		return nil
 	})
 
 	if err != nil {
--- pkg/fs/walk_test.go
+++ pkg/fs/walk_test.go
@@ -5,16 +5,32 @@ import (
 	"testing"
 )
 
-func TestWalk(t *testing.T) {
+func TestWalkFiles(t *testing.T) {
 	found := true
-	files, _ := Walk("test")
+	files, _ := Walk("./testdata", false)
+	for _, f := range files {
+		found = found && (f == "testdata/walk/sub/subsub/two.txt" ||
+			f == "testdata/walk/sub/three.txt" ||
+			f == "testdata/walk/one.txt")
+	}
+
+	if !found {
+		t.Errorf("Couldn't locate expected files")
+	}
+}
+
+func TestWalkFilesAndDirectories(t *testing.T) {
+	found := true
+	files, _ := Walk("./testdata", true)
 	for _, f := range files {
 		fmt.Println(f)
-		found = found && (f == "walk" ||
-			f == "test" ||
-			f == "walk/sub" ||
-			f == "walk/sub/three.txt" ||
-			f == "walk/one.txt")
+		found = found && (f == "testdata" ||
+			f == "testdata/walk" ||
+			f == "testdata/walk/sub" ||
+			f == "testdata/walk/sub/subsub" ||
+			f == "testdata/walk/sub/subsub/two.txt" ||
+			f == "testdata/walk/sub/three.txt" ||
+			f == "testdata/walk/one.txt")
 	}
 
 	if !found {
