9c6e3d11f0b503436a1384387675d9a5fca48960
Chris Hayen <chayen@gmail.com>
2020-10-29 23:56:15 -0400

    Loose api todos

---
+++ pkg/api/api.go
@@ -0,0 +1,17 @@
+package api
+
+/*
+
+	We want to
+	- start a service
+	- have hooks for completion and readiness
+	- monitor health
+	- piece togther diriectories for context
+	- monitor a base image if provided
+	- hook before and after into commands
+	- provide hooks to add commands
+	- listen for Ctrl-C
+	- make RPC or an API available for the desktop app
+	- on branch change hook
+
+*/
